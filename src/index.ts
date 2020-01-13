c0b53c80e2c9440160f943cf5e28a148e0998ad3
Gregor Martynus <gregor@martynus.net>
2020-01-13 12:59:31 -0800

    feat: log before (debug) & after requests (info)

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,35 @@
+import { Octokit } from "@octokit/core";
 import { VERSION } from "./version";
-
-type Octokit = any;
-type Options = {
-  [option: string]: any;
-};
 
 /**
  * @param octokit Octokit instance
  * @param options Options passed to Octokit constructor
  */
-export function requestLog(octokit: Octokit, options: Options) {}
+export function requestLog(octokit: Octokit) {
+  octokit.hook.wrap("request", (request, options) => {
+    octokit.log.debug("request", options);
+
+    const start = Date.now();
+    const requestOptions = octokit.request.endpoint.parse(options);
+    const path = requestOptions.url.replace(options.baseUrl, "");
+
+    return (request as typeof octokit.request)(options)
+      .then(response => {
+        octokit.log.info(
+          `${requestOptions.method} ${path} - ${
+            response.status
+          } in ${Date.now() - start}ms`
+        );
+        return response;
+      })
+
+      .catch(error => {
+        octokit.log.info(
+          `${requestOptions.method} ${path} - ${error.status} in ${Date.now() -
+            start}ms`
+        );
+        throw error;
+      });
+  });
+}
 requestLog.VERSION = VERSION;
